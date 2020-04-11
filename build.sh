2db57a42b8d0ae06ec61304b671195c128dfa15a
DevilTea <ch870814@gmail.com>
2020-04-11 18:13:10 +0800

    Remove deploy script

--- build.sh
+++ build.sh
@@ -19,11 +19,3 @@ cd -
 # replace index.html with temporary index.html
 # remove this line when the main page is online
 rm dist/index.html && cp index.html dist/index.html
-
-# deploy
-cd dist
-git init
-git add -A
-git commit -m 'deploy'
-git push -f https://github.com/COSCUP/2020.git master:gh-pages
-cd -
